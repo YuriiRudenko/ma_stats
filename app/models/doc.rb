550f650575b587698a00adda9b6637155d11f5e0
Yurii Rudenko <yurii.rudenko@masterofcode.com>
2020-09-02 20:43:27 +0300

    security

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -19,11 +19,22 @@ class Doc
 #
 # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
   def authorize
+    File.delete(ENV['CREDENTIALS_PATH']) if File.exists?(ENV['CREDENTIALS_PATH'])
+    File.write(ENV['CREDENTIALS_PATH'], ENV['CREDENTIALS'])
+
+    File.delete(ENV['TOKEN_PATH']) if File.exists?(ENV['TOKEN_PATH'])
+    File.write(ENV['TOKEN_PATH'], ENV['TOKEN'])
+
     client_id = Google::Auth::ClientId.from_file(ENV['CREDENTIALS_PATH'])
     token_store = Google::Auth::Stores::FileTokenStore.new(file: ENV['TOKEN_PATH'])
     authorizer = Google::Auth::UserAuthorizer.new(client_id, ENV['SCOPE'], token_store)
     user_id = 'default'
-    authorizer.get_credentials(user_id)
+    auth = authorizer.get_credentials(user_id)
+
+    File.delete(ENV['CREDENTIALS_PATH']) if File.exists?(ENV['CREDENTIALS_PATH'])
+    File.delete(ENV['TOKEN_PATH']) if File.exists?(ENV['TOKEN_PATH'])
+
+    auth
   end
 
   def get
@@ -32,11 +43,10 @@ class Doc
     service.authorization = authorize
 
     spreadsheet_id = ENV['SPREADSHEET_ID']
-    # response = service.get_spreadsheet_values(spreadsheet_id, 'T1:T1')
-    # response
+    response = service.get_spreadsheet_values(spreadsheet_id, 'M2:M1000')
+    response
 
-    # data = { total_charity: response.values.flatten.first }
-    data = {}
+    data = { total_charity: response.values.flatten.map(&:to_f).sum }
     data[:rating] = (service.get_spreadsheet_values(spreadsheet_id, 'H2:H1000').values || []).flatten.group_by { |i| i }.map { |k, v| [k, v.size] }.to_h
     data[:total] = data[:rating].values.sum
     value = (service.get_spreadsheet_values(spreadsheet_id, 'A2:Z1000').values || []).flatten.count { |v| IT_WORDS.any? { |w| w.in? v } }
